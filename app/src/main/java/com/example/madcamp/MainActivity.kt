1b21f7d891f8c6a9362f1c901c02717046807742
chlehdwon <cookie000215@gmail.com>
2022-06-29 20:48:32 +0900

    Simple tablayout

--- app/src/main/java/com/example/madcamp/MainActivity.kt
+++ app/src/main/java/com/example/madcamp/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.madcamp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.viewpager.widget.ViewPager
+import com.google.android.material.tabs.TabLayout
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val pagerAdapter = FragmentAdapter(supportFragmentManager)
+        val pager = findViewById<ViewPager>(R.id.viewpager)
+        pager.adapter = pagerAdapter
+        val tab = findViewById<TabLayout>(R.id.tablayout)
+        tab.setupWithViewPager(pager)
     }
 }
