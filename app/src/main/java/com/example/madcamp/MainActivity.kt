20b7a21014030773eb820ac907f56b577532f2fc
anista13 <2904003na13*>
2022-07-01 20:53:37 +0900

    Tab2 done

---
+++ app/src/main/java/com/example/madcamp/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.example.madcamp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Toast
+import androidx.recyclerview.widget.RecyclerView
+import androidx.viewpager.widget.ViewPager
+import com.google.android.material.tabs.TabLayout
+import org.json.JSONArray
+import java.io.IOException
+import java.io.InputStream
+import kotlinx.android.synthetic.main.activity_main.*
+
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val pagerAdapter = FragmentAdapter(supportFragmentManager)
+        val pager = findViewById<ViewPager>(R.id.viewpager)
+        pager.adapter = pagerAdapter
+        val tab = findViewById<TabLayout>(R.id.tablayout)
+        tab.setupWithViewPager(pager)
+
+    }
+}
